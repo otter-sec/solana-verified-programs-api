f7815bc61bf9d2912a4e3b29bac91c4ab1cf4b14
qdrs <qdrs@proton.me>
2023-07-25 11:55:46 +0800

    cleanup

--- src/routes.rs
+++ src/routes.rs
@@ -23,7 +23,6 @@ pub fn create_router(db: DbClient) -> Router {
 }
 
 static INDEX_JSON: OnceLock<Value> = OnceLock::new();
-// static i: Value = json!({ "test": "hello" });
 
 fn index() -> Json<Value> {
     let value = INDEX_JSON.get_or_init(||
@@ -108,89 +107,6 @@ async fn verify(
         .into(),
     )
 }
-
-// Route handler for POST /sync_verify which creates a new process to verify the program synchronously
-// async fn handle_verify_sync(
-//     State(db): State<DbClient>,
-//     Json(payload): Json<SolanaProgramBuildParams>,
-// ) -> Json<ApiResponse> {
-//     let verify_build_data = SolanaProgramBuild {
-//         id: uuid::Uuid::new_v4().to_string(),
-//         repository: payload.repository.clone(),
-//         commit_hash: payload.commit_hash.clone(),
-//         program_id: payload.program_id.clone(),
-//         lib_name: payload.lib_name.clone(),
-//         bpf_flag: payload.bpf_flag.unwrap_or(false),
-//         created_at: Utc::now().naive_utc(),
-//     };
-
-//     // First check if the program is already verified
-//     let is_exists = check_is_build_params_exists_already(&db, &payload).await;
-
-//     if let Ok(is_exists) = is_exists {
-//         if is_exists {
-//             return Json(ApiResponse::Error(ErrorResponse {
-//                 status: Status::Error,
-//                 error: "We have already processed this request".to_string(),
-//             }));
-//         }
-//     }
-
-//     // Else insert into database
-//     let insert = db.insert_or_update_build(&verify_build_data).await;
-
-//     match insert {
-//         Ok(_) => {
-//             tracing::info!("Inserted into database");
-//             // Run task in background
-//             let handle = tokio::task::spawn_blocking(move || verify_build(&db, payload));
-
-//             let task = handle.await;
-
-//             if let Ok(res) = task {
-//                 match res.await {
-//                     Ok(verified_program) => {
-//                         tracing::info!("Build verification completed");
-//                         Json(ApiResponse::Success(SuccessResponse::VerifySync(
-//                             VerifySyncResponse {
-//                                 status: Status::Success,
-//                                 is_verified: verified_program.is_verified,
-//                                 on_chain_hash: verified_program.on_chain_hash,
-//                                 executable_hash: verified_program.executable_hash,
-//                                 message: if verified_program.is_verified {
-//                                     "On chain program verified".to_string()
-//                                 } else {
-//                                     "On chain program not verified".to_string()
-//                                 },
-//                             },
-//                         )))
-//                     }
-//                     Err(e) => {
-//                         tracing::error!("Error verifying build: {:?}", e);
-//                         Json(ApiResponse::Error(ErrorResponse {
-//                             status: Status::Error,
-//                             error: format!("unexpected error occurred {:?}", e),
-//                         }))
-//                     }
-//                 }
-//             } else {
-//                 let err_info = format!("unexpected error occurred {:?}", task.err());
-//                 tracing::error!(err_info);
-//                 Json(ApiResponse::Error(ErrorResponse {
-//                     status: Status::Error,
-//                     error: err_info,
-//                 }))
-//             }
-//         }
-//         Err(e) => {
-//             tracing::error!("Error inserting into database: {:?}", e);
-//             Json(ApiResponse::Error(ErrorResponse {
-//                 status: Status::Error,
-//                 error: "unexpected error occurred".to_string(),
-//             }))
-//         }
-//     }
-// }
 
 //  Route handler for GET /status/:address which checks if the program is verified or not
 async fn verify_status(
