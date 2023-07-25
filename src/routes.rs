ee57d40e920cfbc98f05e56271008c58ab6a4451
swaroop-osec <swaroop@osec.io>
2023-07-25 21:00:11 +0530

    Updated sync endpoint

--- src/routes.rs
+++ src/routes.rs
@@ -17,7 +17,8 @@ use std::sync::OnceLock;
 pub fn create_router(db: DbClient) -> Router {
     Router::new()
         .route("/", get(|| async { index() }))
-        .route("/verify", post(verify))
+        .route("/verify", post(verify_async))
+        .route("/verify_sync", post(verify_sync))
         .route("/status/:address", get(verify_status))
         .with_state(db)
 }
@@ -47,7 +48,7 @@ fn index() -> Json<Value> {
 }
 
 // Route handler for POST /verify which creates a new process to verify the program
-async fn verify(
+async fn verify_async(
     State(db): State<DbClient>,
     Json(payload): Json<SolanaProgramBuildParams>,
 ) -> Json<ApiResponse> {
@@ -106,6 +107,75 @@ async fn verify(
         }
         .into(),
     )
+}
+
+async fn verify_sync(
+    State(db): State<DbClient>,
+    Json(payload): Json<SolanaProgramBuildParams>,
+) -> Json<ApiResponse> {
+    let verify_build_data = SolanaProgramBuild {
+        id: uuid::Uuid::new_v4().to_string(),
+        repository: payload.repository.clone(),
+        commit_hash: payload.commit_hash.clone(),
+        program_id: payload.program_id.clone(),
+        lib_name: payload.lib_name.clone(),
+        bpf_flag: payload.bpf_flag.unwrap_or(false),
+        created_at: Utc::now().naive_utc(),
+    };
+
+    // First check if the program is already verified
+    let is_exists = db
+        .check_is_build_params_exists_already(&payload)
+        .await
+        .unwrap_or(false);
+
+    if is_exists {
+        return Json(ApiResponse::Error(ErrorResponse {
+            status: Status::Error,
+            error: "We have already processed this request".to_string(),
+        }));
+    }
+
+    // insert into database
+    if let Err(e) = db.insert_or_update_build(&verify_build_data).await {
+        tracing::error!("Error inserting into database: {:?}", e);
+        return Json(
+            ErrorResponse {
+                status: Status::Error,
+                error: "unexpected error occurred".to_string(),
+            }
+            .into(),
+        );
+    }
+
+    tracing::info!("Inserted into database");
+    //run task in background
+    match verify_build(payload).await {
+        Ok(res) => {
+            let _ = db.insert_or_update_verified_build(&res).await;
+            Json(
+                StatusResponse {
+                    is_verified: res.is_verified,
+                    message: if res.is_verified {
+                        "On chain program verified".to_string()
+                    } else {
+                        "On chain program not verified".to_string()
+                    },
+                }
+                .into(),
+            )
+        }
+        Err(err) => {
+            tracing::error!("Error verifying build: {:?}", err);
+            Json(
+                ErrorResponse {
+                    status: Status::Error,
+                    error: "unexpected error occurred".to_string(),
+                }
+                .into(),
+            )
+        }
+    }
 }
 
 //  Route handler for GET /status/:address which checks if the program is verified or not
