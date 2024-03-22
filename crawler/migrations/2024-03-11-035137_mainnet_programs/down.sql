d715eb03a0a7388b9b902a56631148e040e47a20
Otter-0x4ka5h <0x4ka5h@osec.io>
2024-03-22 13:10:32 +0530

    fix: redis & seperate migration

---
+++ crawler/migrations/2024-03-11-035137_mainnet_programs/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+DROP INDEX mainnet_programs_program_address_uindex;
+DROP TABLE mainnet_programs;
--- schema.rs
+++ schema.rs
@@ -1,35 +1,6 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    solana_program_builds (id) {
-        id -> Varchar,
-        repository -> Varchar,
-        commit_hash -> Nullable<Varchar>,
-        program_id -> Varchar,
-        lib_name -> Nullable<Varchar>,
-        base_docker_image -> Nullable<Varchar>,
-        mount_path -> Nullable<Varchar>,
-        cargo_args -> Nullable<Array<Text>>,
-        bpf_flag -> Bool,
-        created_at -> Timestamp,
-        status -> Varchar,
-    }
-}
-
-diesel::table! {
-    verified_programs (id) {
-        id -> Varchar,
-        program_id -> Varchar,
-        is_verified -> Bool,
-        on_chain_hash -> Varchar,
-        executable_hash -> Varchar,
-        verified_at -> Timestamp,
-        solana_build_id -> Varchar,
-    }
-}
-
-
-diesel::table! {
     mainnet_programs (id) {
         id -> Int4,
         project_name -> Nullable<Varchar>,
@@ -46,5 +17,36 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(verified_programs -> solana_program_builds (solana_build_id));
-diesel::allow_tables_to_appear_in_same_query!(solana_program_builds, verified_programs,);
+diesel::table! {
+    solana_program_builds (program_id) {
+        id -> Varchar,
+        repository -> Varchar,
+        commit_hash -> Nullable<Varchar>,
+        program_id -> Varchar,
+        lib_name -> Nullable<Varchar>,
+        base_docker_image -> Nullable<Varchar>,
+        mount_path -> Nullable<Varchar>,
+        cargo_args -> Nullable<Array<Nullable<Text>>>,
+        bpf_flag -> Bool,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    verified_programs (id) {
+        id -> Varchar,
+        program_id -> Varchar,
+        is_verified -> Bool,
+        on_chain_hash -> Varchar,
+        executable_hash -> Varchar,
+        verified_at -> Timestamp,
+    }
+}
+
+diesel::joinable!(verified_programs -> solana_program_builds (program_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    mainnet_programs,
+    solana_program_builds,
+    verified_programs,
+);
