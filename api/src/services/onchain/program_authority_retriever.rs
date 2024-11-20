8a121530a0acb90197e64f2325b155df4f6df413
swaroop-osec <swaroop@osec.io>
2024-11-20 15:02:05 +0530

    Use the PDA from either the program authority or Osec

---
+++ api/src/services/onchain/program_authority_retriever.rs
@@ -0,0 +1,56 @@
+use std::str::FromStr;
+
+use crate::{Result, CONFIG};
+use solana_account_decoder::parse_bpf_loader::{
+    parse_bpf_upgradeable_loader, BpfUpgradeableLoaderAccountType, UiProgram, UiProgramData,
+};
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::pubkey::Pubkey;
+
+pub async fn get_program_authority(program_id: &Pubkey) -> Result<Option<String>> {
+    let rpc_url = CONFIG.rpc_url.clone();
+    let client = RpcClient::new(rpc_url);
+
+    let program_account_bytes = client.get_account_data(program_id).await?;
+
+    let program_data_account_id = match parse_bpf_upgradeable_loader(&program_account_bytes)? {
+        BpfUpgradeableLoaderAccountType::Program(UiProgram { program_data }) => {
+            Pubkey::from_str(&program_data)?
+        }
+        unexpected => {
+            return Err(crate::errors::ApiError::Custom(format!(
+                "Unexpected program account type: {:?}",
+                unexpected
+            )));
+        }
+    };
+
+    let program_data_account_bytes = client.get_account_data(&program_data_account_id).await?;
+
+    let program_data = match parse_bpf_upgradeable_loader(&program_data_account_bytes)? {
+        BpfUpgradeableLoaderAccountType::ProgramData(UiProgramData { authority, .. }) => authority,
+        unexpected => {
+            return Err(crate::errors::ApiError::Custom(format!(
+                "Unexpected program data account type: {:?}",
+                unexpected
+            )));
+        }
+    };
+    Ok(program_data)
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use solana_sdk::pubkey::Pubkey;
+
+    #[tokio::test]
+    async fn test_get_program_authority() {
+        let program_id = Pubkey::from_str("verifycLy8mB96wd9wqq3WDXQwM4oU6r42Th37Db9fC").unwrap();
+        let result = get_program_authority(&program_id).await;
+        assert!(result.is_ok());
+        let authority = result.unwrap();
+        assert_eq!(authority, Some("9VWiUUhgNoRwTH5NVehYJEDwcotwYX3VgW4MChiHPAqU".to_string()));
+    }
+}
--- api/src/services/onchain/program_metadata_retriever.rs
+++ api/src/services/onchain/program_metadata_retriever.rs
@@ -1,59 +1,11 @@
-use crate::{errors::ApiError, Result, CONFIG};
-use base64::Engine;
+use std::str::FromStr;
+
+use crate::{Result, CONFIG};
 use borsh::{BorshDeserialize, BorshSerialize};
-use reqwest::Client;
-use serde::{Deserialize, Serialize};
-use serde_json::json;
+use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::pubkey::Pubkey;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Params {
-    encoding: String,
-    filters: Vec<Filter>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Filter {
-    memcmp: Memcmp,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Memcmp {
-    offset: u64,
-    bytes: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct RpcRequest {
-    jsonrpc: String,
-    id: u64,
-    method: String,
-    params: Vec<serde_json::Value>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Account {
-    data: (String, String),
-    executable: bool,
-    lamports: u64,
-    owner: String,
-    #[serde(rename = "rentEpoch")]
-    rent_epoch: u64,
-    space: u64,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ResultEntry {
-    account: Account,
-    pubkey: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct RpcResponse {
-    jsonrpc: String,
-    id: u64,
-    result: Option<Vec<ResultEntry>>,
-}
+use super::get_program_authority;
 
 #[derive(BorshDeserialize, BorshSerialize, Debug)]
 pub struct OtterBuildParams {
@@ -67,7 +19,10 @@ pub struct OtterBuildParams {
     bump: u8,
 }
 
-const OTTER_VERIFY_PROGRAMID: &str = "verifycLy8mB96wd9wqq3WDXQwM4oU6r42Th37Db9fC";
+const OTTER_VERIFY_PROGRAMID: Pubkey =
+    solana_sdk::pubkey!("verifycLy8mB96wd9wqq3WDXQwM4oU6r42Th37Db9fC");
+const OTTER_SIGNER_PUBKEY: Pubkey =
+    solana_sdk::pubkey!("9VWiUUhgNoRwTH5NVehYJEDwcotwYX3VgW4MChiHPAqU");
 
 impl OtterBuildParams {
     pub fn is_bpf(&self) -> bool {
@@ -112,70 +67,40 @@ impl OtterBuildParams {
     }
 }
 
+pub async fn get_otter_pda(
+    client: &RpcClient,
+    signer: &Pubkey,
+    program_id_pubkey: &Pubkey,
+) -> Result<OtterBuildParams> {
+    let seeds: &[&[u8]] = &[
+        b"otter_verify",
+        &signer.to_bytes(),
+        &program_id_pubkey.to_bytes(),
+    ];
+    let (pda_account, _) = Pubkey::find_program_address(seeds, &OTTER_VERIFY_PROGRAMID);
+    let account_data = client.get_account_data(&pda_account).await?;
+    let otter_build_params = OtterBuildParams::try_from_slice(&account_data[8..])?;
+    Ok(otter_build_params)
+}
+
 pub async fn get_otter_verify_params(program_id: &str) -> Result<OtterBuildParams> {
     let rpc_url = CONFIG.rpc_url.clone();
+    let client = RpcClient::new(rpc_url.clone());
+    let program_id_pubkey = Pubkey::from_str(program_id)?;
 
-    let client = Client::new();
+    // Try the first PDA based on authority
+    if let Some(authority) = get_program_authority(&program_id_pubkey).await? {
+        let authority_pubkey = Pubkey::from_str(&authority)?;
 
-    let params = Params {
-        encoding: "base64".to_string(),
-        filters: vec![Filter {
-            memcmp: Memcmp {
-                offset: 8,
-                bytes: program_id.to_string(),
-            },
-        }],
-    };
-
-    let request = RpcRequest {
-        jsonrpc: "2.0".to_string(),
-        id: 1,
-        method: "getProgramAccounts".to_string(),
-        params: vec![
-            json!(OTTER_VERIFY_PROGRAMID),
-            serde_json::to_value(params)
-                .map_err(|e| ApiError::Custom(format!("Failed to serialize params {e}")))?,
-        ],
-    };
-
-    let response = client
-        .post(rpc_url)
-        .json(&request)
-        .send()
-        .await
-        .map_err(|_| {
-            ApiError::Custom(
-                "Failed to send request to get Otter Verify params from mainnet".to_string(),
-            )
-        })?;
-
-    let response_json: RpcResponse = response
-        .json()
-        .await
-        .map_err(|_| ApiError::Custom("Failed to parse response from mainnet".to_string()))?;
-
-    if let Some(result) = response_json.result {
-        if let Some(entry) = result.into_iter().next() {
-            let data = base64::prelude::BASE64_STANDARD
-                .decode(entry.account.data.0)
-                .map_err(|e| {
-                    ApiError::Custom(format!("Failed to decode data to base64 from mainnet {e}"))
-                })?;
-            let anchor_account: OtterBuildParams = BorshDeserialize::try_from_slice(&data[8..])
-                .map_err(|e| {
-                    ApiError::Custom(format!("Failed to decode data to Struct from mainnet {e}"))
-                })?;
-            return Ok(anchor_account);
+        if let Ok(otter_build_params) =
+            get_otter_pda(&client, &authority_pubkey, &program_id_pubkey).await
+        {
+            return Ok(otter_build_params);
         }
-        Err(ApiError::Custom(
-            "Failed to find Otter Verify params".to_string(),
-        ))
-    } else {
-        tracing::info!("No results found");
-        Err(ApiError::Custom(
-            "Failed to find Otter Verify params".to_string(),
-        ))
     }
+
+    // Fallback: PDA based on Osec pubkey 
+    get_otter_pda(&client, &OTTER_SIGNER_PUBKEY, &program_id_pubkey).await
 }
 
 #[cfg(test)]
@@ -202,12 +127,12 @@ mod tests {
         let params = data.unwrap();
         let solana_build_params = SolanaProgramBuildParams::from(params);
         assert!(solana_build_params.program_id == "verifycLy8mB96wd9wqq3WDXQwM4oU6r42Th37Db9fC");
-        assert!(solana_build_params.base_image.is_none());
+        assert!(solana_build_params.base_image.is_some());
         assert!(solana_build_params.mount_path.is_none());
         assert!(solana_build_params.lib_name.is_some());
         assert!(solana_build_params.lib_name.unwrap() == "otter_verify");
         assert!(!solana_build_params.bpf_flag.unwrap());
-        assert!(solana_build_params.cargo_args.unwrap().is_empty());
+        assert!(solana_build_params.cargo_args.is_none());
     }
 
     #[tokio::test]
