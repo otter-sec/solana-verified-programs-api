c34e95f7a9610bfa822630b1ffb27942e5dab023
swaroop-osec <swaroop@osec.io>
2024-12-13 14:38:06 +0530

    update sql

--- api/migrations/2023-07-04-082332_init/down.sql
+++ api/migrations/2023-07-04-082332_init/down.sql
@@ -1,5 +1,15 @@
 -- This file should undo anything in `up.sql`
-drop index solana_program_builds_program_id_idx;
-drop index verified_programs_program_id_idx;
-drop table verified_programs;
-drop table solana_program_builds;
+-- Drop indexes
+DROP INDEX IF EXISTS solana_program_builds_program_id_idx;
+DROP INDEX IF EXISTS solana_program_builds_id_idx;
+DROP INDEX IF EXISTS verified_programs_program_id_idx;
+DROP INDEX IF EXISTS verified_programs_solana_build_id_idx;
+DROP INDEX IF EXISTS program_authority_program_id_index;
+DROP INDEX IF EXISTS idx_verified_programs_program_id_is_verified;
+DROP INDEX IF EXISTS idx_solana_program_builds_created_at;
+
+-- Drop tables
+DROP TABLE IF EXISTS build_logs;
+DROP TABLE IF EXISTS program_authority;
+DROP TABLE IF EXISTS verified_programs;
+DROP TABLE IF EXISTS solana_program_builds;
