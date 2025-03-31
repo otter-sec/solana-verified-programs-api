f3997e588a700ac4211e975fdc33ca11fd0acfb4
swaroop-osec <swaroop@osec.io>
2025-03-31 22:53:00 +0530

    Update handle_pda_updates_creations

--- api/src/api/handlers/pda_worker.rs
+++ api/src/api/handlers/pda_worker.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use crate::{
     api::handlers::{async_verify::process_verification, is_authorized},
     db::{
@@ -5,6 +7,7 @@ use crate::{
         DbClient,
     },
     services::{get_on_chain_hash, onchain::OtterBuildParams},
+    CONFIG,
 };
 use axum::{
     extract::State,
@@ -13,6 +16,8 @@ use axum::{
 };
 use borsh::BorshDeserialize;
 use serde_json::Value;
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::pubkey::Pubkey;
 use tracing::{error, info, warn};
 
 pub(crate) async fn handle_pda_updates_creations(
@@ -20,7 +25,7 @@ pub(crate) async fn handle_pda_updates_creations(
     headers: HeaderMap,
     Json(payload): Json<Vec<Value>>,
 ) -> (StatusCode, &'static str) {
-    info!("Received PDA updates/creations request");
+    info!("Received PDA updates/creation event");
 
     // Validate authorization
     if !is_authorized(&headers) {
@@ -39,22 +44,14 @@ pub(crate) async fn handle_pda_updates_creations(
 
     // Process instructions
     for ix in instruction.instructions {
-        let program_id = &ix.accounts[1];
-        info!("Processing upgrade instruction for program: {}", program_id);
-
-        let data = ix.data.as_bytes();
-        let otter_build_params = match OtterBuildParams::try_from_slice(&data[8..]) {
-            Ok(params) => params,
-            Err(e) => {
-                error!("Failed to deserialize PDA data: {}", e);
-                continue;
-            }
-        };
-
-        if let Err(e) = process_pda_upgrade(&db, program_id, otter_build_params).await {
-            error!("Failed to process program upgrade: {}", e);
+        // Only process PDA updates/creations
+        if ix.programId != "verifycLy8mB96wd9wqq3WDXQwM4oU6r42Th37Db9fC" {
             continue;
         }
+        let pda_account = &ix.accounts[0];
+        let program_id = &ix.accounts[2];
+
+        let _ = process_pda_upgrade(&db, program_id, pda_account).await;
     }
 
     (StatusCode::OK, "PDA updates/creations request received")
@@ -63,18 +60,30 @@ pub(crate) async fn handle_pda_updates_creations(
 async fn process_pda_upgrade(
     db: &DbClient,
     program_id: &str,
-    params: OtterBuildParams,
+    pda_account: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let executable_hash = db.get_verified_build(program_id, None).await?;
+    let executable_hash = match db.get_verified_build(program_id, None).await {
+        Ok(data) => data.on_chain_hash,
+        Err(_) => String::default(),
+    };
 
     let onchain_hash = get_on_chain_hash(program_id).await?;
 
-    if onchain_hash != executable_hash.on_chain_hash {
-        info!("Program {} has been upgraded, unverifying", program_id);
+    if onchain_hash != executable_hash {
         db.unverify_program(program_id, &onchain_hash).await?;
         // start new build
-        let signer = params.signer.to_string();
-        let solana_build_params = SolanaProgramBuildParams::from(params);
+        let rpc_client = RpcClient::new(CONFIG.rpc_url.clone());
+        let pda_account_pubkey = Pubkey::from_str(pda_account)?;
+        let params = rpc_client.get_account_data(&pda_account_pubkey).await?;
+        let otter_build_params = match OtterBuildParams::try_from_slice(&params[8..]) {
+            Ok(params) => params,
+            Err(e) => {
+                error!("Failed to deserialize PDA data: {}", e);
+                return Err(e.into());
+            }
+        };
+        let signer = otter_build_params.signer.to_string();
+        let solana_build_params = SolanaProgramBuildParams::from(otter_build_params);
         let _ = process_verification(db.clone(), solana_build_params, signer).await;
         info!("Successfully unverified program {}", program_id);
     } else {
