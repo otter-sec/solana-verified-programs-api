56b21541cd6681e3c86fb518cd2c6ff3c547c822
swaroop-osec <swaroop@osec.io>
2025-07-09 09:52:28 +0530

    Remove crawler service and related files from the project

--- crawler/migrations/2024-03-11-035137_mainnet_programs/down.sql
+++
@@ -1,3 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP INDEX mainnet_programs_program_address_uindex;
-DROP TABLE mainnet_programs;
--- crawler/src/security_txt/mod.rs
+++
@@ -1,9 +0,0 @@
-
-
-/// Constant for the beginning of the security.txt file.
-pub const SECURITY_TXT_BEGIN: &str = "=======BEGIN SECURITY.TXT V1=======\0";
-/// Constant for the end of the security.txt file.
-pub const SECURITY_TXT_END: &str = "=======END SECURITY.TXT V1=======\0";
-
-
-pub mod parser;
